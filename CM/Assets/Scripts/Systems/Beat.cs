4a19897e37f471bdde163a53425638fe8d382339
Seblax <48605789+Seblax@users.noreply.github.com>
2026-02-18 17:28:07 +0100

    Config Menu

---
+++ CM/Assets/Scripts/Systems/Beat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Beat : MonoBehaviour
+{
+    [SerializeField]
+    float _timer;
+    float _secondsPerBeat;
+
+    [Header("Mussic Sync Settings")]
+    public float bpm = 135f;
+    public int beat;
+    public int offset;
+
+    [Header("Event")]
+    public UnityEvent _event;
+
+
+    void Start()
+    {
+        _timer = -offset * (60f / bpm);
+    }
+
+    void Update()
+    {
+        if (_event == null) return;
+        if (beat <= 0) return;
+
+        _secondsPerBeat = 60f / (bpm/beat);
+
+        _timer += Time.deltaTime;
+
+        if (_timer >= _secondsPerBeat)
+        {
+            _timer -= _secondsPerBeat;
+            _event?.Invoke();
+        }   
+    }
+}
