82a5dbd55ad86edadf3f631f0151b8299cc59d0b
Seblax <48605789+Seblax@users.noreply.github.com>
2026-02-25 15:25:09 +0100

    Save changes before merging to main

--- CM/Assets/Scripts/Systems/Beat.cs
+++ CM/Assets/Scripts/Systems/Beat.cs
@@ -3,8 +3,14 @@ using UnityEngine.Events;
 
 public class Beat : MonoBehaviour
 {
+<<<<<<< HEAD
     public float _timer;
     public float _secondsPerBeat;
+=======
+    [SerializeField]
+    float _timer;
+    float _secondsPerBeat;
+>>>>>>> origin/main
 
     [Header("Mussic Sync Settings")]
     public float bpm = 135f;
@@ -15,18 +21,27 @@ public class Beat : MonoBehaviour
     public UnityEvent _event;
 
 
+<<<<<<< HEAD
     public void Start()
+=======
+    void Start()
+>>>>>>> origin/main
     {
         _timer = -offset * (60f / bpm);
     }
 
+<<<<<<< HEAD
     protected virtual void Update()
+=======
+    void Update()
+>>>>>>> origin/main
     {
         if (_event == null) return;
         if (beat <= 0) return;
 
         _secondsPerBeat = 60f / (bpm/beat);
 
+<<<<<<< HEAD
         Timer();
         Event();
     }
@@ -43,5 +58,14 @@ public class Beat : MonoBehaviour
             _timer = 0;
             _event?.Invoke();
         }
+=======
+        _timer += Time.deltaTime;
+
+        if (_timer >= _secondsPerBeat)
+        {
+            _timer -= _secondsPerBeat;
+            _event?.Invoke();
+        }   
+>>>>>>> origin/main
     }
 }
